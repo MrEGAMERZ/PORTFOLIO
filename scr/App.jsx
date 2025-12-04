4497809645e4f360dfd1512f6bf369ac26db5eac
MrEGAMERZ <147334329+MrEGAMERZ@users.noreply.github.com>
2025-12-04 10:42:11 +0530

    test

--- scr/App.jsx
+++ scr/App.jsx
@@ -1,34 +1,36 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
-export default function App() {
+function App() {
   return (
-    <div className="w-full bg-black text-white overflow-x-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-black text-white">
       <Navbar />
-
+      
       <section id="hero">
         <Hero />
       </section>
-
+      
       <section id="about">
         <About />
       </section>
-
+      
       <section id="experience">
         <Experience />
       </section>
-
+      
       <section id="projects">
         <Projects />
       </section>
-
+      
       <section id="contact">
         <Contact />
       </section>
     </div>
-  );
+  )
 }
+
+export default App
